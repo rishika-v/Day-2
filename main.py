6afe4e63aa66a18a886d38d8f72700b83e58a3f2
RishikaVadlamud <70661830+rishika-v@users.noreply.github.com>
2021-07-06 15:06:39 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+# < -less than, <=
+# > - greater than, >=
+# == - equal to
+# != - not equal to
+
+# and- both statements have to work
+# or- either one statement has to work
+# not - opposite of statement
+
+#boolean
+
+import random
+
+ComputerNumber = random.randint(0,10)
+
+maxguesses = 3
+Win = False
+Play = True
+
+while Play == True:
+  while maxguesses > 0:
+    num = input
+    if <
+    elif >
+    else:
+      Win = True
