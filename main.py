57614d3c1ef924f6bd302d86e2a9391ac83d6e08
Rishika Vadlamudi <70661830+rishika-v@users.noreply.github.com>
2021-07-06 15:48:34 +0000

    finished code

--- main.py
+++ main.py
@@ -18,12 +18,34 @@ Win = False
 Play = True
 
 while Play == True:
+
   while maxguesses > 0:
-    num = input("Enter a number (0-10): ")
+    num = int(input("Enter a number (0-10): "))
     if num < ComputerNumber:
       print("Your guess is lesser than the number")
+      maxguesses = maxguesses -1
+      print(str(maxguesses) + " guesses left")
     elif num > ComputerNumber:
       print("Your guess is greater than the number")
+      maxguesses = maxguesses -1
+      print(str(maxguesses) + " guesses left")
     else:
       print("You got it!")
       Win = True
+      maxguesses = 0
+
+
+
+  if Win == True:
+      print("Congratulations!")
+  else:
+      print("No more tries")
+      print ("The number was: " + str(ComputerNumber))
+
+  answer = input("Do you want to play again? Y/N ")
+  if answer.upper() == "N":
+      print("Game Over")
+      Play = False
+  else:
+      Win = False
+      maxguesses = 5
